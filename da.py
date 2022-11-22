fce19ba02ad685997a749c86546b590ada4e75e6
Nitin Jaiswal <nitin.jazz@gmail.com>
2022-11-21 21:33:13 -0500

    ok

--- da.py
+++ da.py
@@ -1,32 +1,44 @@
-class Paths:
-    def __init__(self, paths):
-        self.paths = paths
+# Predefined paths variables:
+# | DA.paths.working_dir:  dbfs:/mnt/dbacademy-users/nitinjazz@gmail.com/data-engineering-with-databricks
+# | DA.paths.user_db:      dbfs:/mnt/dbacademy-users/nitinjazz@gmail.com/data-engineering-with-databricks/database.db
+# | DA.paths.datasets:     dbfs:/mnt/dbacademy-datasets/data-engineering-with-databricks/v02
+# | DA.paths.checkpoints:  dbfs:/mnt/dbacademy-users/nitinjazz@gmail.com/data-engineering-with-databricks/_checkpoints
+# | DA.paths.kafka_events: dbfs:/mnt/dbacademy-datasets/data-engineering-with-databricks/v02/ecommerce/raw/events-kafka
 
-    @property
-    def datasets(self):
-        #print('nitin',data_set)
-        return self.paths
+
+class Paths:
+    def __init__(self):
+        None
+
+        datasets = None
+        kafka_events = None
+        working_dir = None
+        user_db = None
+        datasets = None
+        checkpoints = None
+
 
 class DA:
-    def __init__(self, schema_name,path):
-        self.schema_name = schema_name
-        self.path=path
-        self.paths=Paths(self.path)
+    def __init__(self):
+        self.paths = Paths()
 
-    #return data_sets()
 
-path = "dbfs:/mnt/dbacademy-datasets/data-engineering-with-databricks/v02/"
-da=DA("analytics",path)
+da = DA()
 
-print(da.schema_name)
-print(da.paths.datasets)
-
+da.paths.datasets = "dbfs:/mnt/dbacademy-datasets/data-engineering-with-databricks/v02"
 da.paths.kafka_events = f"{da.paths.datasets}/ecommerce/raw/events-kafka"
-print(da.paths.kafka_events)
+da.paths.working_dir = "dbfs:/mnt/data_engineering"
+da.paths.user_db = f"{da.paths.working_dir}/analytics.db"
+da.paths.checkpoints = f"{da.paths.working_dir}/_checkpoints"
 
+print('da.paths.datasets :', da.paths.datasets)
+print('da.paths.kafka_events:', da.paths.kafka_events)
+print('da.paths.working_dir:', da.paths.working_dir)
+print('da.paths.user_db:', da.paths.user_db)
+print('da.paths.checkpoints:', da.paths.checkpoints)
 
-#spark.conf.set('da.schema_name', f"{da.schema_name}")
-#spark.conf.get('da.schema_name')
+# spark.conf.set('da.schema_name', f"{da.schema_name}")
+# spark.conf.get('da.schema_name')
 
 # COMMAND ----------
 
