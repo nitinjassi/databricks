da38691511d98496c47e05c289c32fcc99955987
nitinjazz@gmail.com <nitinjazz@gmail.com>
2022-11-22 03:19:24 +0000

    ok

--- setup.py
+++ setup.py
@@ -17,27 +17,46 @@ spark.conf.set('da.paths.kafka_events',f"{da_paths_datasets}/ecommerce/raw/event
 
 # COMMAND ----------
 
+# Predefined paths variables:
+# | DA.paths.working_dir:  dbfs:/mnt/dbacademy-users/nitinjazz@gmail.com/data-engineering-with-databricks
+# | DA.paths.user_db:      dbfs:/mnt/dbacademy-users/nitinjazz@gmail.com/data-engineering-with-databricks/database.db
+# | DA.paths.datasets:     dbfs:/mnt/dbacademy-datasets/data-engineering-with-databricks/v02
+# | DA.paths.checkpoints:  dbfs:/mnt/dbacademy-users/nitinjazz@gmail.com/data-engineering-with-databricks/_checkpoints
+# | DA.paths.kafka_events: dbfs:/mnt/dbacademy-datasets/data-engineering-with-databricks/v02/ecommerce/raw/events-kafka
+
+
 class Paths:
     def __init__(self):
         None
 
-    datasets=None
-    kafka_events=None
+        datasets = None
+        kafka_events = None
+        working_dir = None
+        user_db = None
+        datasets = None
+        checkpoints = None
+
 
 class DA:
     def __init__(self):
-        self.paths=Paths()
+        self.paths = Paths()
 
 da=DA()
 
 da.paths.datasets="dbfs:/mnt/dbacademy-datasets/data-engineering-with-databricks/v02"
-print('da.paths.datasets :', da.paths.datasets)
-
 da.paths.kafka_events = f"{da.paths.datasets}/ecommerce/raw/events-kafka"
-print('da.paths.kafka_events:',da.paths.kafka_events)
+da.paths.working_dir = "dbfs:/mnt/data_engineering"
+da.paths.user_db = f"{da.paths.working_dir}/analytics.db"
+da.paths.checkpoints = f"{da.paths.working_dir}/_checkpoints"
 
-#spark.conf.set('da.schema_name', f"{da.schema_name}")
-#spark.conf.get('da.schema_name')
+print('da.paths.datasets :', da.paths.datasets)
+print('da.paths.kafka_events:', da.paths.kafka_events)
+print('da.paths.working_dir:', da.paths.working_dir)
+print('da.paths.user_db:', da.paths.user_db)
+print('da.paths.checkpoints:', da.paths.checkpoints)
+
+# spark.conf.set('da.schema_name', f"{da.schema_name}")
+# spark.conf.get('da.schema_name')
 
 # COMMAND ----------
 
